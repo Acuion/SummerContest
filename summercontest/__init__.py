39534274c5b89bf34a34b67c5e657892322fe0af
Yuriy <natarur@yandex.com>
2018-06-02 19:41:16 +0300

    pg instance fix & debugging

--- summercontest/__init__.py
+++ summercontest/__init__.py
@@ -2,9 +2,12 @@ from postgres import Postgres
 from summercontest import privatedata
 from flask import Flask
 
-PGI = Postgres(privatedata.pgconnectionstring)
+PGI = None
 
 def pgInstance():
+    global PGI
+    if PGI is None:
+        PGI = Postgres(privatedata.pgconnectionstring)
     return PGI
 
 app = Flask(__name__)
@@ -12,3 +15,7 @@ app = Flask(__name__)
 from summercontest import cache
 from summercontest import groupsProvider
 from summercontest import frontend
+
+from werkzeug.debug import DebuggedApplication
+app.debug = True
+app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
