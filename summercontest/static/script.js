4f2bc1616f439276aef23a54ea45b6e72c7a5c20
Yuriy <natarur@yandex.com>
2018-06-02 19:27:57 +0300

    total solved

--- summercontest/static/script.js
+++ summercontest/static/script.js
@@ -20,6 +20,8 @@ function appendToTable(element, rank) {
 }
 
 function loadGroups() {
+    let totalacmp = 0, totaltimus = 0, totalcfdiv1 = 0, totalcfdiv23 = 0;
+
     $('#toappend').empty();
     appendToTable({name: 'Загрузка ─=≡Σ((( つ◉◡◔)つ', sum: 0, data: {acmp: 0, timus: 0, cfdiv1: 0, cfdiv23: 0}, div: ''}, '<img src="static/Gear-1s-42px.svg"></img>');
     $.getJSON("/groups", function( data ) {
@@ -28,6 +30,14 @@ function loadGroups() {
         data.forEach(element => {
             ps = ps.then(() => $.getJSON(`/solvedCached?acmp=${element['acmp']}&timus=${element['timus']}&cf=${element['cf']}`, function( data ) {
                 let newElement = {name: element['fio'], data: data, div: element['div'], sum: data['acmp'] + data['timus'] * 2 + data['cfdiv1'] * 10 + data['cfdiv23'] * 5};
+                totalacmp += data['acmp'];
+                totaltimus += data['timus'];
+                totalcfdiv1 += data['cfdiv1'];
+                totalcfdiv23 += data['cfdiv23'];
+                $('#acmp-total').text(`Acmp решено (${totalacmp})`);
+                $('#timus-total').text(`Timus решено (${totaltimus})`);
+                $('#cfdiv1-total').text(`CF.Div1 решено (${totalcfdiv1})`);
+                $('#cfdiv23-total').text(`CF.Div2/3 решено (${totalcfdiv23})`);
                 table.push(newElement);
                 appendToTable(newElement, '');
             })).catch(() => {
