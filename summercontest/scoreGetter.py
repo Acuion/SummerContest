be0dddc0b4a97f8720c2b09976242f2737087e90
Yuriy <natarur@yandex.com>
2018-06-03 21:05:46 +0300

    timus fix

--- summercontest/scoreGetter.py
+++ summercontest/scoreGetter.py
@@ -14,10 +14,11 @@ def getAcmpSolved(idStr):
 
 def getTimusSolved(idStr):
     prof = requests.get('http://acm.timus.ru/author.aspx?id=' + idStr)
-    if 'No problems solved' in prof.text:
-        return 0
     soup = BeautifulSoup(prof.text, 'html.parser')
-    acs = soup.select('.author_stats_value')[1]
+    statsTdsList = soup.select('.author_stats_value')
+    if not statsTdsList:
+        return 0
+    acs = statsTdsList[1]
     acs = acs.text.split(' ')[0]
     return int(acs)
 
