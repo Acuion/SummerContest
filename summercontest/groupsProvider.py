b053de9900bd97b19dc338872de15df0bc8d5e55
Yuriy <natarur@yandex.com>
2019-06-20 23:41:28 +0300

    zero skips

--- summercontest/groupsProvider.py
+++ summercontest/groupsProvider.py
@@ -4,10 +4,12 @@ import json
 
 @app.route("/groups")
 def groupApi():
-    with open('/var/suco/groups.txt', 'r', encoding='utf-8') as f:
+    with open('/var/suco/groups-2019.txt', 'r', encoding='utf-8') as f:
         rawGroups = f.readlines()
         groups = []
         for line in rawGroups:
             cid, f, i, o, div, timus, acmp = line.split()
+            if '0' == timus or '0' == acmp:
+                continue
             groups.append({'id': cid, 'fio': '{} {}'.format(i,f), 'div': div, 'timus': timus, 'acmp': acmp})
         return json.dumps(groups)
